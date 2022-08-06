e7e45e9800788e59b0d997f727637f086f104e19
Vaibhav Sharma <yavsvaibhav@gmail.com>
2022-08-06 20:54:28 +0530

    updates for deployment

---
+++ app.py
@@ -0,0 +1,33 @@
+import backend.dashboard as dashboard
+import backend.auth as auth
+import backend.project as project
+import backend.db as db
+import backend.roles as roles
+from flask import Flask
+import os
+
+
+extra_dirs = [
+    "../frontend/html"
+]
+
+extra_files = extra_dirs[:]
+for extra_dir in extra_dirs:
+    for dirname, dirs, files in os.walk(extra_dir):
+        for filename in files:
+            filename = os.path.join(dirname, filename)
+            if os.path.isfile(filename):
+                extra_files.append(filename)
+
+app = Flask(__name__, template_folder="../frontend/html",
+            static_folder='../frontend/static/')
+app.config['SECRET_KEY'] = '755203b56dc7dccc4d7ee10503232e31'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+db.init_app(app)
+app.register_blueprint(auth.bp)
+app.register_blueprint(dashboard.bp)
+app.register_blueprint(project.bp)
+app.register_blueprint(roles.bp)
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, extra_files=extra_files)
